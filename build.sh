e10301529f19ded7576459eb41de40cebe515bc6
Blon Td <offk614@gmail.com>
2026-05-15 17:26:50 -0400

    Ensure build script sets executable permissions

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 echo "Building KeyRemapper..."
 clang -fobjc-arc -framework Foundation -framework ApplicationServices -framework Carbon KeyRemapper.m -o "KeyRemapper"
 if [ $? -eq 0 ]; then
+    chmod +x "KeyRemapper"
     echo "Successfully built KeyRemapper."
     echo "To run it, type: ./KeyRemapper"
 else
